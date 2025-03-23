033467fd41b41933fdf8dc9469cad5b4c997bcc9
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2025-03-23 14:04:55 +0100

    remove old code

--- test-project/build.gradle.kts
+++ test-project/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     id("eu.darkcube.darkcube")
 }
 
-//group = "eu.darkcube.system.test"
-
 gradle.taskGraph.whenReady {
     allTasks.filterIsInstance<JavaExec>().forEach {
         it.setExecutable(it.javaLauncher.get().executablePath.asFile.absolutePath)
@@ -21,25 +19,15 @@ tasks.withType<JavaExec>().configureEach {
     javaLauncher = javaToolchains.launcherFor(java.toolchain)
 }
 
-//val remap = configurations.create("remap")
 val embedStep1 = configurations.create("embedStep1")
 val embedStep2 = configurations.create("embedStep2")
 
 
 repositories {
-//    mavenLocal()
     mavenCentral()
     maven("https://libraries.minecraft.net")
 }
 
-//dependencies {
-//    remap("com.google.code.gson:gson:2.10.1")
-//    remap(libs.bundles.adventure)
-//    remap("org.jetbrains:annotations:24.1.0")
-//    remap("com.mojang:brigadier:1.0.18")
-//
-////    implementation("eu.darkcube.system:libs:1.0-SNAPSHOT")
-//}
 dependencies {
     embedStep1(libs.brigadier)
     embedStep1(libs.gson)
@@ -50,8 +38,6 @@ dependencies {
         exclude(group = "com.google.code.gson")
     }
 }
-
-//sourceRemapper.remap(remap, "eu.darkcube.system.test.libs", configurations.named("implementation"))
 
 val step2 = sourceRemapper.remap(embedStep2, "eu.darkcube.system.test.libs", configurations.named("api"))
 val step1 = sourceRemapper.remap(embedStep1, "eu.darkcube.system.test.libs", configurations.named("api"))
@@ -76,8 +62,6 @@ publishing {
         register<MavenPublication>("test") {
 
             from(component)
-//            from(components["java"])
-//            from(conf.component.configureJava(sourceSets.main, tasks.jar).component)
         }
     }
 }
